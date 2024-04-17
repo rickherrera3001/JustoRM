cc5e7cfa0680e830c34f99d89c1fa3535122e119
rickherrera3001 <rihr.3001@gmail.com>
2024-04-17 17:39:17 -0600

    create files JustoViewModel and JustoView

---
+++ Justo RM/Moduls/JustoView.swift
@@ -0,0 +1,8 @@
+//
+//  JustoView.swift
+//  Justo RM
+//
+//  Created by Ricardo Developer on 17/04/24.
+//
+
+import Foundation
---
+++ Justo RM/Moduls/JustoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  JustoViewModel.swift
+//  Justo RM
+//
+//  Created by Ricardo Developer on 17/04/24.
+//
+
+import Foundation
