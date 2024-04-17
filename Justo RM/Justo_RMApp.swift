5150f9b84c0636314d37a04b5a6f0d81bf7daa69
rickherrera3001 <rihr.3001@gmail.com>
2024-04-17 15:55:01 -0600

    Initial Commit

---
+++ Justo RM/Justo_RMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Justo_RMApp.swift
+//  Justo RM
+//
+//  Created by Ricardo Developer on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Justo_RMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
